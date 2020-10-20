d24bacf3225695a73dab20e83a08664b3bb7b796
Weslen Almeida <weslen-a@hotmail.com>
2020-10-20 01:14:31 -0300

    pushing the exec 05

--- exec05/src/com/java/maromo/fatecmm/Main.java
+++ exec05/src/com/java/maromo/fatecmm/Main.java
@@ -1,14 +1,32 @@
 package com.java.maromo.fatecmm;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
         Cartela ct = new Cartela();
+        Scanner sc = new Scanner(System.in);
+        int op = 0;
 
-        ct.imprimirCartelas();
+        do {
+            System.out.println("");
+            System.out.printf("1 - Novo Sorteio%n2 - Imprimir Sorteio%n3 - Sair%nEscolha:%n");
+            op = sc.nextInt();
+            if (op == 1) {
+                System.out.println("Sorteio realizado com sucesso!!!");
+                System.out.println("Escolha a opcao 2 para imprimir o resultado.");
+                ct = new Cartela() ;
+            } else if (op == 2) {
+                ct.imprimirCartelas();
+            } else if (op == 3) {
+                System.out.println("Ate logo!");
+            } else {
+                System.out.println("Valor Invalido!!!");
+            }
+        } while (op != 3);
 
-
-
+        sc.close();
     }
 }
